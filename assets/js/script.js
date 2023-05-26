e7407abf35831ee85fa38610195a52cf8610f79b
izzeddinayyad <azeedenayyad@gmail.com>
2023-05-26 20:02:08 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// document.write("hello");
+// document.getElementById("result").innerHTML="hello js";
+// document.getElementById("btn"),onclick =function(){
+//     document.getElementById("result").innerHTML="hello js";
+// };
+// console.log("test");
+// alert("test");
+// var y =[1,3,6,5];
+var test ={
+    name :"ahmed",
+    age :"15"
+}
+console.log(test["name"]);
